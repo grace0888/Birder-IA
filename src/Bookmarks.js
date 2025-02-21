56bd323fd132bf8ef11a8b105af2e7c60de5ef7c
Grace <gracejhuh08@gmail.com>
2025-02-20 23:14:02 -0600

    Created navigation bar for Bookmarks and Uploaded Images pages as well!

--- src/Bookmarks.js
+++ src/Bookmarks.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Box } from "@chakra-ui/react";
+import { Flex } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
+import { Link } from '@chakra-ui/react';
 
 function Bookmarks(){
     return (
-        <div>
-            <h1>Bookmarks</h1>
-        </div>
+        <Flex direction="column">
+            <Box minH="10vh" background="bg" p={5} width="100%" color="black">
+                <Flex gap = "5" direction="row" justify="space-between" align="center">
+                    <Text fontSize="2xl" fontWeight="bold">{""}
+                        <Link href="http://localhost:3000/home">BIRDER</Link>
+                    </Text>
+                    <Flex gap={6}>
+                        <Link href="http://localhost:3000/home">Home</Link>
+                        <Link href="http://localhost:3000/uploaded-images">Uploaded Images</Link>
+                        <Link href="http://localhost:3000/bookmarks" fontWeight="bold" borderBottom="2px solid black">Bookmarks</Link>
+                    </Flex>
+                </Flex>
+            </Box>
+            <Box minH="90vh" background="cyan.700" width="100%" color="white">
+                Enter bookmarks code here
+            </Box>
+        </Flex>
     )
 }
 
--- src/UploadedImages.js
+++ src/UploadedImages.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Box } from "@chakra-ui/react";
+import { Flex } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
+import { Link } from '@chakra-ui/react';
 
 function UploadedImages(){
     return (
-        <div>
-            <h1>Uploaded Images</h1>
-        </div>
+        <Flex direction="column">
+            <Box minH="10vh" background="bg" p={5} width="100%" color="black">
+                <Flex gap = "5" direction="row" justify="space-between" align="center">
+                    <Text fontSize="2xl" fontWeight="bold">{""}
+                        <Link href="http://localhost:3000/home">BIRDER</Link>
+                    </Text>
+                    <Flex gap={6}>
+                        <Link href="http://localhost:3000/home">Home</Link>
+                        <Link href="http://localhost:3000/uploaded-images" fontWeight="bold" borderBottom="2px solid black">Uploaded Images</Link>
+                        <Link href="http://localhost:3000/bookmarks">Bookmarks</Link>
+                    </Flex>
+                </Flex>
+            </Box>
+            <Box minH="90vh" background="cyan.700" width="100%" color="white">
+                Enter images code here
+            </Box>
+        </Flex>
     )
 }
 
