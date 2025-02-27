bf81d2ebe41facb890430fcab20dad2fbef77784
Grace <gracejhuh08@gmail.com>
2025-02-27 09:44:06 -0600

    made base for locationresults and speciesresults

--- src/SpeciesResults.js
+++ src/SpeciesResults.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import { Flex } from '@chakra-ui/react';
+import { Box } from "@chakra-ui/react";
+import { Text } from '@chakra-ui/react';
+import { Link } from '@chakra-ui/react';
+import { Input } from '@chakra-ui/react';
+import { InputGroup } from "./components/ui/input-group"
+import { LuSearch } from "react-icons/lu"
 
 function SpeciesResults(){
     return (
-        <div>
-            <h1>Species Results</h1>
-        </div>
+       <Flex direction="column">
+            <Box minH="10vh" background="bg" p={5} width="100%" color="black">
+                <Flex gap = "5" direction="row" justify="space-between" align="center">
+                    <Text fontSize="2xl" fontWeight="bold">{""}
+                        <Link href="http://localhost:3000/home">BIRDER</Link>
+                    </Text>
+                    <Flex gap={6}>
+                        <InputGroup
+                            flex="1"
+                            startElement={<LuSearch />}
+                        >
+                            <Input
+                                placeholder = "Enter a species"
+                                variant="outline"
+                                bg="white"
+                                htmlSize={80}
+                                color="black"
+                                width="auto"
+                                size="lg"
+                            />
+                        </InputGroup>
+                    </Flex>
+                </Flex>
+            </Box>
+            <Box minH="90vh" background="cyan.700" width="100%" color="white">
+                Enter species results code here
+            </Box>
+        </Flex>
     )
 }
 
