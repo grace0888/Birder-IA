3d8654eab650e9deee7d8afe4024be8c52707742
Grace <gracejhuh08@gmail.com>
2025-02-27 09:47:04 -0600

    made detailed info base

--- src/DetailedInformation.js
+++ src/DetailedInformation.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Flex } from '@chakra-ui/react';
+import { Box } from "@chakra-ui/react";
+import { Text } from '@chakra-ui/react';
+import { Link } from '@chakra-ui/react';
 
 function DetailedInformation(){
     return (
-        <div>
-            <h1>Detailed Info</h1>
-        </div>
+       <Flex direction="column">
+            <Box minH="10vh" background="bg" p={5} width="100%" color="black">
+                <Flex gap = "5" direction="row" justify="space-between" align="center">
+                    <Text fontSize="2xl" fontWeight="bold">{""}
+                        <Link href="http://localhost:3000/home">BIRDER</Link>
+                    </Text>
+                    <Flex gap={6}>
+                        Enter detailed info stuff here
+                    </Flex>
+                </Flex>
+            </Box>
+            <Box minH="90vh" background="cyan.700" width="100%" color="white">
+                Enter detailed info code here
+            </Box>
+        </Flex>
     )
 }
 
