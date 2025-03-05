437bb0ee070c74935a65e3585e0f82484d0484c5
Grace <gracejhuh08@gmail.com>
2025-03-05 10:16:29 -0600

    Made bird data

---
+++ src/data/birds.js
@@ -0,0 +1,119 @@
+export const birdsData = [
+    {
+        id: 1,
+        name: "Northern Harrier",
+        scientificName: "Circus hudsonius",
+        displayImage: "https://www.allaboutbirds.org/guide/assets/photo/64805151-480px.jpg", // need a list of image URLs, not just one
+        habitat: "The Northern Harrier typically is found in open areas such as grasslands, marshes, and croplands. They prefer westlands with tall, native, dense vegetation as well as lightly grazed pastures. They have been observed breeding from Alaska to Mexico.",
+        migration: "Winters are spent in Central America and northern South America, and warmer months are spent more northward. They migrate alone during the daytime.",
+        behavior: "These birds are low flyers and tend to glide. They circle areas to search for prey, and nest in hidden spots on the ground.",
+    },
+    {
+        id: 2,
+        name: "Pine Siskin",
+        scientificName: "Spinus pinus",
+        displayImage: "https://www.allaboutbirds.org/guide/assets/photo/306421661-480px.jpg", // need a list of image URLs
+        habitat: "These birds are found in forests, fields, and the suburbs and are mainly found in North America.",
+        migration: "Semi-open areas are preferred during winter, where they can be found in western United States such as California and Arizona. During breeding season, Pine Siskins are found in the northern part of the continent.",
+        behavior: "Pine siskins are foragers and look for seeds in the topmost canopies of trees. They can sometimes be found upside down on small branches, searching through cones.",
+    },
+    {
+        id: 3,
+        name: "Cedar Waxwing",
+        scientificName: "Bombycilla cedrorum",
+        displayImage: "https://www.birdsandblooms.com/wp-content/uploads/2022/10/GettyImages-1136115716.jpg?fit=680%2C510", // need a list of image URLs
+        habitat: "Cedar Waxwings can be found in a variety of environments, including orchards, farms, the suburbs, and woodlands. They are typically seen in southern United States and have been observed down to Colombia.",
+        migration: "Winters are spent in southern United States and northern South America. During breeding season, these birds travel into northern United States. They migrate in flocks at night.",
+        behavior: "It's common to see Cedar Waxwings in flocks as they are very social animals. They feed on berries and hunt water insects.",
+    },
+    {
+        id: 4,
+        name: "Ruddy Duck",
+        scientificName: "Oxyura jamaicensis",
+        displayImage: "https://www.allaboutbirds.org/guide/assets/photo/302124271-720px.jpg", // need a list of image URLs
+        habitat: "These ducks can be found in rivers, lakes, and wetlands. They are found in North America.",
+        migration: "Breeding months are spent in Western Canada while winters are spent in Southern United States and Mexico. However some Ruddy Ducks in the Caribbean and South America don't migrate. When migration occurs, it happens mostly at night in small flocks.",
+        behavior: "These birds reside in shallow waters and spend most of their life in water. They sleep while floating.",
+    },
+    {
+        id: 5,
+        name: "Harlequin Duck",
+        scientificName: "Histrionicus histrionicus",
+        displayImage: "https://www.allaboutbirds.org/guide/assets/photo/63898051-480px.jpg", // need a list of image URLs
+        habitat: "",
+        migration: "",
+        behavior: "",
+    },
+    {
+        id: 6,
+        name: "Fulvous Whistling Duck",
+        scientificName: "Dendrocygna bicolor",
+        displayImage: "https://www.allaboutbirds.org/guide/assets/photo/302323931-480px.jpg", // need a list of image URLs
+        habitat: "",
+        migration: "",
+        behavior: "",
+    },
+    {
+        id: 7,
+        name: "Muscovy Duck",
+        scientificName: "Cairina moschata",
+        displayImage: "https://www.allaboutbirds.org/guide/assets/photo/299692021-1280px.jpg", // need a list of image URLs
+        habitat: "",
+        migration: "",
+        behavior: "",
+    },
+    {
+        id: 8,
+        name: "Long-tailed Duck",
+        scientificName: "Clangula hyemalis",
+        displayImage: "https://www.allaboutbirds.org/guide/assets/photo/63902971-240px.jpg", // need a list of image URLs
+        habitat: "",
+        migration: "",
+        behavior: "",
+    },
+    {
+        id: 9,
+        name: "Orange-crowned Warbler",
+        scientificName: "Leiothlypis celata",
+        displayImage: "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/296785521/1800", // need a list of image URLs
+        habitat: "",
+        migration: "",
+        behavior: "",
+    },
+    {
+        id: 10,
+        name: "White-winged Dove",
+        scientificName: "Zenaida asiatica",
+        displayImage: "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/308431191/1800", // need a list of image URLs
+        habitat: "",
+        migration: "",
+        behavior: "",
+    },
+    {
+        id: 11,
+        name: "Hermit Thrush",
+        scientificName: "Catharus guttatus",
+        displayImage: "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/303450651/1800", // need a list of image URLs
+        habitat: "",
+        migration: "",
+        behavior: "",
+    },
+    {
+        id: 12,
+        name: "White-crowned Sparrow",
+        scientificName: "Zonotrichia leucophrys",
+        displayImage: "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/64978031/1800", // need a list of image URLs
+        habitat: "",
+        migration: "",
+        behavior: "",
+    },
+    {
+        id: 13,
+        name: "Yellow-bellied Sapsucker",
+        scientificName: "Sphyrapicus varius",
+        displayImage: "https://cdn.download.ams.birds.cornell.edu/api/v1/asset/297655981/1800", // need a list of image URLs
+        habitat: "",
+        migration: "",
+        behavior: "",
+    }
+];
